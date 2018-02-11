1f929558a2c4ca4bb5bf9cb591634d554d3368e4
Benjamin Davis <greendartfrog@gmail.com>
2018-02-10 20:28:31 -0500

    Made some working vision..!

---
+++ vision_app/src/getContours.cpp
@@ -0,0 +1,165 @@
+#include "getContours.h"
+
+namespace grip {
+
+getContours::getContours() {
+}
+/**
+* Runs an iteration of the pipeline and updates outputs.
+*/
+void getContours::Process(cv::Mat& source0){
+	//Step Blur0:
+	//input
+	cv::Mat blurInput = source0;
+	BlurType blurType = BlurType::MEDIAN;
+	double blurRadius = 5.0;  // default Double
+	blur(blurInput, blurType, blurRadius, this->blurOutput);
+	//Step HSL_Threshold0:
+	//input
+	cv::Mat hslThresholdInput = blurOutput;
+	double hslThresholdHue[] = {20.0, 40.0};
+	double hslThresholdSaturation[] = {90.0, 255.0};
+	double hslThresholdLuminance[] = {30.0, 255.0};
+	hslThreshold(hslThresholdInput, hslThresholdHue, hslThresholdSaturation, hslThresholdLuminance, this->hslThresholdOutput);
+	//Step Mask0:
+	//input
+	cv::Mat maskInput = source0;
+	cv::Mat maskMask = hslThresholdOutput;
+	mask(maskInput, maskMask, this->maskOutput);
+	//Step Find_Contours0:
+	//input
+	cv::Mat findContoursInput = hslThresholdOutput;
+	bool findContoursExternalOnly = false;  // default Boolean
+	findContours(findContoursInput, findContoursExternalOnly, this->findContoursOutput);
+	//Step Convex_Hulls0:
+	//input
+	std::vector<std::vector<cv::Point> > convexHullsContours = findContoursOutput;
+	convexHulls(convexHullsContours, this->convexHullsOutput);
+}
+
+/**
+ * This method is a generated getter for the output of a Blur.
+ * @return Mat output from Blur.
+ */
+cv::Mat* getContours::GetBlurOutput(){
+	return &(this->blurOutput);
+}
+/**
+ * This method is a generated getter for the output of a HSL_Threshold.
+ * @return Mat output from HSL_Threshold.
+ */
+cv::Mat* getContours::GetHslThresholdOutput(){
+	return &(this->hslThresholdOutput);
+}
+/**
+ * This method is a generated getter for the output of a Mask.
+ * @return Mat output from Mask.
+ */
+cv::Mat* getContours::GetMaskOutput(){
+	return &(this->maskOutput);
+}
+/**
+ * This method is a generated getter for the output of a Find_Contours.
+ * @return ContoursReport output from Find_Contours.
+ */
+std::vector<std::vector<cv::Point> >* getContours::GetFindContoursOutput(){
+	return &(this->findContoursOutput);
+}
+/**
+ * This method is a generated getter for the output of a Convex_Hulls.
+ * @return ContoursReport output from Convex_Hulls.
+ */
+std::vector<std::vector<cv::Point> >* getContours::GetConvexHullsOutput(){
+	return &(this->convexHullsOutput);
+}
+	/**
+	 * Softens an image using one of several filters.
+	 *
+	 * @param input The image on which to perform the blur.
+	 * @param type The blurType to perform.
+	 * @param doubleRadius The radius for the blur.
+	 * @param output The image in which to store the output.
+	 */
+	void getContours::blur(cv::Mat &input, BlurType &type, double doubleRadius, cv::Mat &output) {
+		int radius = (int)(doubleRadius + 0.5);
+		int kernelSize;
+		switch(type) {
+			case BOX:
+				kernelSize = 2 * radius + 1;
+				cv::blur(input,output,cv::Size(kernelSize, kernelSize));
+				break;
+			case GAUSSIAN:
+				kernelSize = 6 * radius + 1;
+				cv::GaussianBlur(input, output, cv::Size(kernelSize, kernelSize), radius);
+				break;
+			case MEDIAN:
+				kernelSize = 2 * radius + 1;
+				cv::medianBlur(input, output, kernelSize);
+				break;
+			case BILATERAL:
+				cv::bilateralFilter(input, output, -1, radius, radius);
+				break;
+        }
+	}
+	/**
+	 * Segment an image based on hue, saturation, and luminance ranges.
+	 *
+	 * @param input The image on which to perform the HSL threshold.
+	 * @param hue The min and max hue.
+	 * @param sat The min and max saturation.
+	 * @param lum The min and max luminance.
+	 * @param output The image in which to store the output.
+	 */
+	//void hslThreshold(Mat *input, double hue[], double sat[], double lum[], Mat *out) {
+	void getContours::hslThreshold(cv::Mat &input, double hue[], double sat[], double lum[], cv::Mat &out) {
+		cv::cvtColor(input, out, cv::COLOR_BGR2HLS);
+		cv::inRange(out, cv::Scalar(hue[0], lum[0], sat[0]), cv::Scalar(hue[1], lum[1], sat[1]), out);
+	}
+
+		/**
+		 * Filter out an area of an image using a binary mask.
+		 *
+		 * @param input The image on which the mask filters.
+		 * @param mask The binary image that is used to filter.
+		 * @param output The image in which to store the output.
+		 */
+		void getContours::mask(cv::Mat &input, cv::Mat &mask, cv::Mat &output) {
+			mask.convertTo(mask, CV_8UC1);
+			cv::bitwise_xor(output, output, output);
+			input.copyTo(output, mask);
+		}
+
+	/**
+	 * Finds contours in an image.
+	 *
+	 * @param input The image to find contours in.
+	 * @param externalOnly if only external contours are to be found.
+	 * @param contours vector of contours to put contours in.
+	 */
+	void getContours::findContours(cv::Mat &input, bool externalOnly, std::vector<std::vector<cv::Point> > &contours) {
+		std::vector<cv::Vec4i> hierarchy;
+		contours.clear();
+		int mode = externalOnly ? cv::RETR_EXTERNAL : cv::RETR_LIST;
+		int method = cv::CHAIN_APPROX_SIMPLE;
+		cv::findContours(input, contours, hierarchy, mode, method);
+	}
+
+	/**
+	 * Compute the convex hulls of contours.
+	 *
+	 * @param inputContours The contours on which to perform the operation.
+	 * @param outputContours The contours where the output will be stored.
+	 */
+	void getContours::convexHulls(std::vector<std::vector<cv::Point> > &inputContours, std::vector<std::vector<cv::Point> > &outputContours) {
+		std::vector<std::vector<cv::Point> > hull (inputContours.size());
+		outputContours.clear();
+		for (size_t i = 0; i < inputContours.size(); i++ ) {
+			cv::convexHull(cv::Mat((inputContours)[i]), hull[i], false);
+		}
+		outputContours = hull;
+	}
+
+
+
+} // end grip namespace
+
--- vision_app/src/main.cpp
+++ vision_app/src/main.cpp
@@ -5,33 +5,114 @@
 #include <llvm/raw_ostream.h>
 #include <cscore.h>
 #include <opencv2/core.hpp>
+#include "getContours.h"
 
 int main() {
   auto inst = NT_GetDefaultInstance();
+  //auto distanceEntry = 0;
   (void)inst;
+  //NT_GetDefaultInstance().StartClientTeam(4534);
+  nt::StartClientTeam(inst, 4534, 1735); // 1735 is default port.
   cs::UsbCamera camera{"usbcam", 0};
-  camera.SetVideoMode(cs::VideoMode::kMJPEG, 320, 240, 30);
+  int width = 320;
+  int height = 240;
+  double fieldOfViewX = (16 * (68.5 / (sqrt(337))));
+  double fieldOfViewY = (9 * (68.5 / (sqrt(337))));
+  // converts standard aspect ratio of the camera into degrees
+  int fps = 10;
+  int debugCountInny = 0;
+  int debugCountOutty = 0;
+  grip::getContours GetContours = grip::getContours();
+  camera.SetVideoMode(cs::VideoMode::kMJPEG, width, height, fps);
   cs::MjpegServer mjpegServer{"httpserver", 8081};
   mjpegServer.SetSource(camera);
   cs::CvSink cvsink{"cvsink"};
   cvsink.SetSource(camera);
-  cs::CvSource cvsource{"cvsource", cs::VideoMode::kMJPEG, 320, 240, 30};
-  cs::MjpegServer cvMjpegServer{"cvhttpserver", 8082};
-  cvMjpegServer.SetSource(cvsource);
+  cs::CvSource filterimage{"filterimage", cs::VideoMode::kMJPEG, width, height, fps};
+  cs::MjpegServer fiMjpegServer{"fihttpserver", 8082};
+  fiMjpegServer.SetSource(filterimage);
+  cs::CvSource finale{"finale", cs::VideoMode::kMJPEG, width, height, fps};
+  cs::MjpegServer cvMjpegServer{"finalhttpserver", 8083};
+  cvMjpegServer.SetSource(finale);
+
+  nt::SetEntryValue ("/CameraPublisher/usbcam/streams", nt::Value::MakeStringArray(llvm::ArrayRef<std::string> ({"http://10.45.34.55:8081/?action=stream"})));
+  nt::SetEntryValue ("/CameraPublisher/filtered/streams", nt::Value::MakeStringArray(llvm::ArrayRef<std::string> ({"http://10.45.34.55:8082/?action=stream"})));
+  nt::SetEntryValue ("/CameraPublisher/final/streams", nt::Value::MakeStringArray(llvm::ArrayRef<std::string> ({"http://10.45.34.55:8083/?action=stream"})));
+
 
   cv::Mat test;
   cv::Mat flip;
-  for (;;) {
+  for (;;){
     uint64_t time = cvsink.GrabFrame(test);
     if (time == 0) {
       std::cout << "error: " << cvsink.GetError() << std::endl;
       continue;
     }
+
+    //std::cout << "Outer = " << ++debugCountOutty << std::endl;
+
+    GetContours.Process(test);
     std::cout << "got frame at time " << time << " size " << test.size()
               << std::endl;
-    cv::flip(test, flip, 0);
-    cvsource.PutFrame(flip);
-  }
+    //cv::flip(test, flip, 0);
+
+
+    int largestArea = 0;
+    int largestAreaIndex = -1;
+
+    std::vector<std::vector<cv::Point> > contours = *GetContours.GetFindContoursOutput();
+    // std::vector<std::vector<cv::Point> >::const_iterator i;
+
+    // for (i=contours.begin(); i!=contours.end(); ++i) {
+        // *i =
+        // double area = contourArea(*i);
+    for (unsigned int i = 0; i < contours.size(); i++) {
+        //convexHull(contours[i], contours[i]);
+        double area = contourArea(contours[i]);
+     //std::cout << "Inner = " << ++debugCountInny << std::endl;
+       if (area > largestArea) {
+            largestArea = area;
+            largestAreaIndex = i;
+        }
+
+        //std::cout << "largestAreaIndex: " << largestAreaIndex << std::endl;
+        }
+    if (largestAreaIndex > -1) {
+        //std::cout << "largestArea: " << largestArea << std::endl;
+
+        std::vector<cv::Point> nearContour = contours[largestAreaIndex];
+
+        cv::Scalar color( rand()&255, rand()&255, rand()&255 );
+        drawContours(test, contours, largestAreaIndex, color);
+        //delete(&contours);
+        cv::Rect nearBox = boundingRect(nearContour);
+
+        rectangle(test, nearBox.tl(), nearBox.br(), color);
+
+        std::cout << "Top left: (" << nearBox.tl().x << ", " << nearBox.tl().y << ")" << std::endl;
+        std::cout << "Top right: (" << nearBox.br().x << ", " << nearBox.tl().y << ")" << std::endl;
+        std::cout << "Bottom left: (" << nearBox.tl().x << ", " << nearBox.br().y << ")" << std::endl;
+        std::cout << "Bottom right: (" << nearBox.br().x << ", " << nearBox.br().y << ")" << std::endl;
+
+        cv::Point center = cv::Point((nearBox.br().x - ((nearBox.br().x - nearBox.tl().x) / 2)), (nearBox.br().y - ((nearBox.br().y - nearBox.tl().y) / 2)));
+        std::cout << "Center: (" << center.x << ", " << center.y << ")" << std::endl;
+
+        int pixelsOffCenterX = ((width / 2) - center.x);
+        int pixelsOffCenterY = ((height / 2) - center.y);
+
+        double magicRatio = (11.0 / (nearBox.br().y - nearBox.tl().y));
+        double inchesOffCenterX = (pixelsOffCenterX * magicRatio);
+
+        std::cout << "Inches off center: " << inchesOffCenterX << std::endl;
+        //std::shared_ptr<nt::Value> NTOffCenter = (new nt::Value())
+        //NTOffCenter.reset(inchesOffCenterX);
+        nt::SetEntryValue ("vision/InchesOffCenter", nt::Value::MakeDouble(inchesOffCenterX));
+
+    }
+    finale.PutFrame(test);
+    filterimage.PutFrame(*GetContours.GetMaskOutput());
+
+    }
 
   return 0;
 }
